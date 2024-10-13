a33dc6bbc855add493402b4f0cc9417bb2bc08ea
Tamas Michelberger <tomi.michel@gmail.com>
2024-10-13 22:25:32 +0200

    Fix formatting for concurrent writes to STDOUT

--- log.go
+++ log.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/fatih/color"
@@ -12,5 +13,12 @@ func infof(format string, args ...any) {
 	if !strings.HasSuffix(format, "\n") {
 		format += "\n"
 	}
-	bold.Printf("===> "+format, args...)
+
+	// NOTE: use Sprintf to create the color formatting string and use
+	// fmt.Print to write it to STDOUT in one go. color.Printf uses separate
+	// writes for setting the color, writing the string, and resetting the
+	// color. Separate writes to STDOUT allows concurrent writes to interleave
+	// with each other which can result in text being formatted which should
+	// not be formatted.
+	fmt.Print(bold.Sprintf("===> "+format, args...))
 }
